3b65fbb1c3caa0d2c380981d55b3ed4c6cfb6b47
Pedro Tasca <pedrotasca1@gmail.com>
2023-05-28 22:47:39 +0100

    implemented react-spring and generated scrolling effect successfully"

--- personal-portfolio-may/src/components/Hero/Hero.js
+++ personal-portfolio-may/src/components/Hero/Hero.js
@@ -2,17 +2,40 @@
 import React from 'react';
 import './Hero.scss'
 import HeroImage from "./HeroImage.png";
-import { AiOutlineArrowDown } from 'react-icons/ai';
+import Arrow from '../../Arrow.png';
+import { useSpring, animated } from '@react-spring/web'
 
 
 function Hero() {
+  const scrollTo = (target) => {
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const arrowAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { tension: 30, friction: 20 },
+  });
+
   return (
     <section className='hero'>
       <div className='heroContainer'>
         <h1>CREATIVE <br></br>DEVELOPER</h1>
-        <img src={HeroImage}></img>
+        <img src={HeroImage} className='heroImage'></img>
         <div className='bottom'>
-          <h1><AiOutlineArrowDown/></h1>
+          <animated.div
+            className={'arrow'}
+            style={arrowAnimation}
+            onClick={() => scrollTo('#howlingMoon')}
+          >
+            <img src={Arrow} />
+          </animated.div>
           <p>
             I SUPPORT DESIGNERS
             <br></br>
